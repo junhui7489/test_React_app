23fabd4847c21d9d4fe4287c5514f5671284c65a
Goh Jun Hui <junhui7489@hotmail.com>
2022-05-10 22:04:05 +0800

    Added route for home page

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, BrowserRouter} from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ForgotPasswordPage from './pages/ForgotPasswordPage'
+import HomePage from './pages/HomePage';
 
 
 const Router = () => (
@@ -12,6 +13,7 @@ const Router = () => (
             <Route path="/log_in" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/forgot_password" element={<ForgotPasswordPage />} />
+            <Route path="/home" element={<HomePage />} />
         </Routes>
     </BrowserRouter>
 );
