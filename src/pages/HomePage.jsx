8df142ea3569b6a178e3c26806b36a85478f2f52
Goh Jun Hui <junhui7489@hotmail.com>
2022-05-12 22:30:27 +0800

    add react fragment, lifecycle methods and appbar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,8 @@ import "../App.css";
 import logo from "../assets/logo-social.png";
 import { Link } from "react-router-dom";
 import ReactTable from './components/ReactTable';
-
+import ButtonAppBar from './components/Toolbar';
+import "../App.css";
 
 class HomePage extends React.Component{
     constructor(props){
@@ -15,26 +16,50 @@ class HomePage extends React.Component{
       const response = await fetch('https://jsonplaceholder.typicode.com/photos');
       const photos = await response.json();
       // waits until the request completes...
-      console.log(photos);
+      //console.log(photos);
       return photos;
     }
     
     componentDidMount() {
       this.fetchJson().then((data)=>{
-        console.log(data);
+        //console.log(data);
         this.setState({
-          info: data
+          info: data,
+          new_info: []
         })
       });
 
     }
 
+    componentWillUnmount() {
+      console.log("Home Page unmounted!");
+      this.setState({info: [], new_info: []});
+    }
+
+    static getDerivedStateFromProps(props, state) {    
+      console.log("Get Derived State", state);
+  
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+      console.log("Get Snapshot before Update", prevState.info, this.state.info);
+      //this.setState({new_info: this.state.info})
+    }
+
+    componentDidUpdate(){
+      console.log("Updated!");  
+    }
+  
+
     render() {
       return (
-        <div>
+        <React.Fragment>
+          <ButtonAppBar className="appbar" />
+          <div>
           <h2>Main Menu</h2>
           <ReactTable data={this.state.info} />
-        </div>
+          </div>
+        </React.Fragment>
       );
     }
 }
